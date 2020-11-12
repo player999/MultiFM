6da41a66006c04304cc0a867ee09272699bd95a7
Taras Zakharchenko <taras.zakharchenko@gmail.com>
2020-11-12 23:37:53 +0200

    Add mixing

--- single_fm.m
+++ single_fm.m
@@ -2,6 +2,7 @@ clear;
 f = fopen('data100mhz.bin', 'r');
 Fs = 20000000;
 AFs = 44100;
+station = 5e6;
 decimator = 50;
 
 raw_values = fread(f,'int8');
@@ -11,7 +12,8 @@ mat = double(raw_values) + 1;
 Q = mat(2:2:end);
 I = mat(1:2:end);
 
-[If,Qf] = filter_ml('lowpass', 256, 20e6, 100e3, decimator, I, Q);
+[Im,Qm] = mixer_ml(Fs, station, I, Q);
+[If,Qf] = filter_ml('lowpass', 256, Fs, 100e3, decimator, Im, Qm);
 omega = atan_ml(If,Qf);
 y = diff_ml(omega);
 [z1,~] = filter_ml('lowpass', 256, Fs/decimator, AFs/2, (Fs/decimator)/AFs, y, y);
