3a8c51ccad951f4a83eb56e5dfbc91709b02a953
Taras Zakharchenko <taras.zakharchenko@gmail.com>
2020-11-15 20:24:17 +0200

    Mixer performance upgrade

--- mixer.cpp
+++ mixer.cpp
@@ -1,5 +1,6 @@
 #include "mixer.hpp"
 #include <cmath>
+#include <exception>
 #include "errno.h"
 
 using namespace DSP;
@@ -45,7 +46,60 @@ namespace DSP
     {
         return mix<float>(fs, f, input_i, input_q, output_i, output_q, len);
     }
+
+    template <class T> Error Mixer<T>::resizeFrame(size_t length)
+    {
+        T current_angle = 0.0;
+        size_t start = sine_i.size();
+
+        sine_i.resize(length);
+        sine_q.resize(length);
+
+        for(size_t ii = start; ii < length; ii++)
+        {
+            sine_i[ii] = sin(current_angle);
+            sine_q[ii] = cos(current_angle);
+
+            current_angle += step;
+            if(current_angle > (2 * M_PI))
+            {
+                current_angle -= 2 * M_PI;
+            }
+        }
+
+        return SUCCESS;
+    }
+
+    template <class T> Mixer<T>::Mixer(double fs, double f, size_t initial_length)
+    {
+        step = 2 * M_PI * f / fs;
+        if(SUCCESS != resizeFrame(initial_length))
+        {
+            std::__throw_runtime_error("Failed to fill initial frame");
+        }
+    }
+
+    template <class T> Error Mixer<T>::mix(const T *input_i, const T *input_q, T *output_i, T *output_q, size_t len)
+    {
+        if(len > sine_i.size())
+        {
+            if(SUCCESS != resizeFrame(len))
+            {
+                return FAIL;
+            }
+        }
+
+        for(size_t ii = 0; ii < len; ii++)
+        {
+            output_i[ii] = (sine_i[ii] * input_i[ii]) - (sine_q[ii] * input_q[ii]);
+            output_q[ii] = (sine_i[ii] * input_q[ii]) + (sine_q[ii] * input_i[ii]);
+        }
+        return SUCCESS;
+    }
 }
+
+template class Mixer<float>;
+template class Mixer<double>;
 
 extern "C"
 {
