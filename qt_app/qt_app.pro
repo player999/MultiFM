5cefea39fcd4d3118bf74204b1487f1ec09fa2d5
Taras Zakharchenko <taras.zakharchenko@gmail.com>
2020-12-04 23:33:09 +0200

    RF device search

--- qt_app/qt_app.pro
+++ qt_app/qt_app.pro
@@ -27,6 +27,8 @@ HEADERS += \
 FORMS += \
     main_window.ui
 
+LIBS += -lrtlsdr -lhackrf
+
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
--- qt_app/receiver_locator.cpp
+++ qt_app/receiver_locator.cpp
@@ -1,4 +1,6 @@
 #include "receiver_locator.h"
+#include <rtl-sdr.h>
+#include <libhackrf/hackrf.h>
 
 Receiver::Receiver(ReceiverType type, QString serial):
     _type(type), _serial(serial)
@@ -38,12 +40,38 @@ QString Receiver::toString()
 
 static void find_rtlsdrs(QList<Receiver> &rcvrs)
 {
-    rcvrs.append(Receiver(ReceiverType::RTLSDR, "0"));
+    uint32_t number_of_devices = rtlsdr_get_device_count();
+    char manufacturer_name[1024];
+    char product_name[1024];
+    char serial_number[1024];
+    for(uint32_t ii = 0; ii < number_of_devices; ii++)
+    {
+        int ret = rtlsdr_get_device_usb_strings(ii,
+            manufacturer_name, product_name, serial_number);
+        if(0 == ret)
+        {
+            rcvrs.append(Receiver(ReceiverType::RTLSDR, serial_number));
+        }
+
+    }
 }
 
 static void find_hackrfs(QList<Receiver> &rcvrs)
 {
-    rcvrs.append(Receiver(ReceiverType::HACKRF, "1"));
+    int ret;
+    hackrf_device_list_t* devlist;
+    ret = hackrf_init();
+    if(ret) return;
+    devlist = hackrf_device_list();
+    if(devlist)
+    {
+        for(int ii = 0; ii < devlist->devicecount; ii++)
+        {
+            rcvrs.append(Receiver(ReceiverType::HACKRF, devlist->serial_numbers[ii]));
+        }
+        (void)hackrf_device_list_free(devlist);
+    }
+    (void)hackrf_exit();
 }
 
 void find_receivers(QList<Receiver> &rcvrs)
